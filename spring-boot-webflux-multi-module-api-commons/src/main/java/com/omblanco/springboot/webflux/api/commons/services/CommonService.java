6c0d3ea7b993f8948ee96f8c9011b78c202d50be
Oscar Martinez <omblanco@gmail.com>
2020-12-11 13:32:09 +0100

    estructura inicial para el módulo model de acceso a datos

---
+++ spring-boot-webflux-multi-module-api-commons/src/main/java/com/omblanco/springboot/webflux/api/commons/services/CommonService.java
@@ -0,0 +1,42 @@
+package com.omblanco.springboot.webflux.api.commons.services;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+/**
+ * Interfaz del servicio genérico
+ * @author oscar.martinezblanco
+ *
+ * @param <D> Clase DTO
+ * @param <E> Clase Entity
+ * @param <K> Clase del ID
+ */
+public interface CommonService<D, E, K> {
+
+    /**
+     * Recupera un listado con todos los dtos
+     * @return Flux de dtos
+     */
+    Flux<D> findAll();
+    
+    /**
+     * Busca un dto por la clave
+     * @param id Clave
+     * @return Dto
+     */
+    Mono<D> findById(K id);
+    
+    /**
+     * Guarda un dto
+     * @param dto dto
+     * @return Dto resultado de la operación
+     */
+    Mono<D> save(D dto);
+    
+    /**
+     * Elimina un dto
+     * @param dto dto a eliminar
+     * @return Resultado de la operación
+     */
+    Mono<Void> delete(D dto);
+}
---
+++ spring-boot-webflux-multi-module-api-mongo-app/src/main/java/com/omblanco/springboot/webflux/api/mongo/app/model/repositories/UserRepository.java
@@ -0,0 +1,30 @@
+package com.omblanco.springboot.webflux.api.mongo.app.model.repositories;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.core.aggregation.Aggregation;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+import com.omblanco.springboot.webflux.api.mongo.app.model.entity.User;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveMongoRepository<User, String>, CustomUserRepository {
+    
+    /**
+     * Busca un usuario por email
+     * @param email email
+     * @return Usuario
+     */
+    Mono<User> findByEmail(String email);
+    
+    /**
+     * No funciona Aggregation en ReactiveMongoRepository
+     * @param aggregation
+     * @param pageable
+     * @return
+     */
+    Flux<User> findBy(Aggregation aggregation, Pageable pageable);
+    Mono<Long> countBy(Aggregation aggregation);
+    
+}
